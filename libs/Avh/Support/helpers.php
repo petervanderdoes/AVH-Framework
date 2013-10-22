c5c840aa4ca5235f2b82d4a80689db733167ca86
Peter van der Does <github@avirtualhome.com>
2013-10-22 09:24:15 -0400

    Move method to function.
    Class is now obsolete

--- libs/Avh/Support/helpers.php
+++ libs/Avh/Support/helpers.php
@@ -13,3 +13,20 @@ if (!function_exists('avh_starts_with')) {
         return Avh\Support\Str::startsWith($haystack, $needle);
     }
 }
+
+if (!function_exists('avh_is_valid_url')) {
+    /**
+     * Determine if the given path is a valid URL.
+     *
+     * @param  string $path
+     * @return bool
+     */
+    function avh_is_valid_url($path)
+    {
+        if (avh_starts_with($path, array('#', '//', 'mailto:', 'tel:'))) {
+            return true;
+        }
+    
+        return filter_var($path, FILTER_VALIDATE_URL) !== false;
+    }
+}
