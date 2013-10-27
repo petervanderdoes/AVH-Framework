e061eb3e2f3421ee0b2922dd05ff096549e29a87
Peter van der Does <github@avirtualhome.com>
2013-10-27 11:18:35 -0400

    Add obfuscation of email.

--- libs/Avh/Html/HtmlBuilder.php
+++ libs/Avh/Html/HtmlBuilder.php
@@ -130,7 +130,9 @@ class HtmlBuilder
             $title = $email;
         }
 
-        return '<a href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;' . $email . '"' . $this->attributes($attributes) . '>' . $title . '</a>';
+        $email = $this->obfuscate('mailto:') . $email;
+
+        return '<a href="' . $email . '"' . $this->attributes($attributes) . '>' . esc_html($title) . '</a>';
     }
 
     /**
@@ -210,5 +212,36 @@ class HtmlBuilder
             return $key . '="' . esc_attr($value) . '"';
         }
         ;
+    }
+
+    /**
+     * Obfuscate a string to prevent spam-bots from sniffing it.
+     *
+     * @param string $value
+     * @return string
+     */
+    public function obfuscate($value)
+    {
+        $safe = '';
+
+        foreach (str_split($value) as $letter) {
+            // To properly obfuscate the value, we will randomly convert each letter to
+            // its entity or hexadecimal representation, keeping a bot from sniffing
+            // the randomly obfuscated letters out of the string on the responses.
+            switch (rand(1, 3)) {
+                case 1:
+                    $safe .= '&#' . ord($letter) . ';';
+                    break;
+
+                case 2:
+                    $safe .= '&#x' . dechex(ord($letter)) . ';';
+                    break;
+
+                case 3:
+                    $safe .= $letter;
+            }
+        }
+
+        return $safe;
     }
 }
