0fb58854b4ddfd96df5a451499a193398dcca3dc
Peter van der Does <github@avirtualhome.com>
2014-02-11 21:01:01 -0500

    Start using travis-ci

---
+++ tests/test-unit-tests.php
@@ -0,0 +1,6 @@
+<?php
+class AVH_Framework_Test_Unit_Tests extends PHPUnit_Framework_TestCase {
+	function test_true_is_true() {
+		$this->assertTrue( true ); // Yay, your unit tests are running!
+	}
+}
