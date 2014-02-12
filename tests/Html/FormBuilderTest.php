df58afcae869842c58db46ddd96f1757c94221fc
Peter van der Does <github@avirtualhome.com>
2014-02-11 22:56:39 -0500

    Tests are needed for testing Travis-CI.
    To test if the implementation of Travis-CI works we need to add some
    tests. These test are regular tests for the Framwework. We're not
    implementing all tests for the entire Framework or even all the test for
    the class, these will be done at a later stage.

--- tests/Html/FormBuilderTest.php
+++ tests/Html/FormBuilderTest.php
@@ -1,7 +1,7 @@
 <?php
-
 use Avh\Html\FormBuilder;
 use Avh\Html\HtmlBuilder;
+
 
 class FormBuilderTest extends WP_UnitTestCase  {
 
@@ -17,8 +17,27 @@ class FormBuilderTest extends WP_UnitTestCase  {
 
 	public function testOpeningForm()
 	{
-		$form1 = $this->formBuilder->open(array('method' => 'GET'));
+		$form1 = $this->formBuilder->open('http://localhost/foo', array('method' => 'GET'));
 
-		$this->assertEquals('<form method="GET" action="http://localhost/foo" accept-charset="UTF-8">', $form1);
+		$this->assertEquals('<form action="http://localhost/foo" method="GET">', $form1);
 	}
+
+	public function testClosingForm() {
+	    $this->assertEquals('</form>', $this->formBuilder->close());
+	}
+
+	public function testOpenTable() {
+	    $this->assertEquals("\n<table class='form-table'>\n", $this->formBuilder->openTable());
+	}
+
+	public function testCloseTable() {
+	    $this->assertEquals("\n</table>\n", $this->formBuilder->closeTable());
+	}
+
+	public function testText() {
+	    $form1 = $this->formBuilder->text('Label text', 'label_name', 'value', array('maxlength' => '32'));
+        $form1_expect ="\n<label for=\"label_name\">Label text</label>\n<input type=\"text\" id=\"label_name\" name=\"label_name\" value=\"value\" maxlength=\"32\" />'";
+	    $this->assertEquals($form1_expect, $form1);
+	}
+
 }
