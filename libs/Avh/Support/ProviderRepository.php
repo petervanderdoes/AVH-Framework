5c922efe8f1c80dd4daf3df6455b767621dbf1f0
Peter van der Does <github@avirtualhome.com>
2015-02-27 19:19:09 -0500

    Fix documentation

--- libs/Avh/Support/ProviderRepository.php
+++ libs/Avh/Support/ProviderRepository.php
@@ -8,7 +8,7 @@ class ProviderRepository
     /**
      * The application implementation.
      *
-     * @var \Illuminate\Contracts\Foundation\Application
+     * @var ApplicationContract
      */
     protected $app;
     /**
