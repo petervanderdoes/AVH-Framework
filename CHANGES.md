f2bd8783902b9a0acb35753089c3094f5b2ee338
Peter van der Does <github@avirtualhome.com>
2015-03-14 22:30:01 -0400

    Preparation for new development cycle after release 1.4.2

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,8 @@
 # Changelog
 
+### 1.4.3-dev.1
+* Preparation for new development cycle.
+
 ### 1.4.2
 * Preparation for new development cycle.
 
--- avh-framework.php
+++ avh-framework.php
@@ -3,7 +3,7 @@
  * Plugin Name: AVH Framework
  * Plugin URI: http://blog.avirtualhome.com/wordpress-plugins
  * Description: This plugin was written to support all other AVH plugins
- * Version: 1.4.2
+ * Version: 1.4.3-dev.1
  * Author: Peter van der Does
  * Author URI: http://blog.avirtualhome.com/
  * Copyright 2013-2014 Peter van der Does (email : peter@avirtualhome.com)
